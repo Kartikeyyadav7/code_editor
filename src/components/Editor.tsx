ece8e315943146de703a3b7bb3d9b3619ecb1ec6
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2022-01-17 17:58:30 +0530

    got editor working

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
-import Editor, { DiffEditor, useMonaco, loader } from '@monaco-editor/react';
+import React, { useState } from 'react';
+import Editor from '@monaco-editor/react';
+import files from '../utils/files';
 
 const MonacoEditor = () => {
+  const [fileName, setFileName] = useState('script.js');
+  const file = files[fileName];
   return (
-    <Editor
-      height="90vh"
-      defaultLanguage="javascript"
-      defaultValue="// some comment"
-    />
+    <>
+      <button
+        disabled={fileName === 'script.js'}
+        onClick={() => setFileName('script.js')}
+      >
+        script.js
+      </button>
+      <button
+        disabled={fileName === 'style.css'}
+        onClick={() => setFileName('style.css')}
+      >
+        style.css
+      </button>
+      <button
+        disabled={fileName === 'index.html'}
+        onClick={() => setFileName('index.html')}
+      >
+        index.html
+      </button>
+      <Editor
+        height="80vh"
+        theme="vs-dark"
+        path={file.name}
+        defaultLanguage={file.language}
+        defaultValue={file.value}
+      />
+    </>
   );
 };
 
