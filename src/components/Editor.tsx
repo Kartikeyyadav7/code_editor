14e0ca8808d3f63ca4be23b0688fc2c1ed67285b
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2022-01-18 20:09:37 +0530

    done some changes fetching data url directly and showing the result in the editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,13 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Editor from '@monaco-editor/react';
 import files from '../utils/files';
 
 const MonacoEditor = () => {
   const [fileName, setFileName] = useState('script.js');
+  const [number, setNumber] = useState(1);
   const file = files[fileName];
+
+  let res;
+  const fetchHtml = async () => {
+    res = await fetch('http://localhost:5000/');
+    const text = await res.text();
+    console.log(text);
+    setFileName(text);
+  };
+  // const fetchCss = async () => {
+  //   const res = await fetch('http://localhost:5000/style.css');
+  //   const text = await res.text();
+  //   console.log(text);
+  // };
+
+  // const fetchJS = async () => {
+  //   const res = await fetch('http://localhost:5000/script.js');
+  //   const text = await res.text();
+  //   console.log(text);
+  // };
+  const handleClick = () => {
+    setNumber(number + 1);
+  };
+
+  useEffect(() => {
+    fetchHtml();
+    // fetchCss();
+    // fetchJS();
+  }, [number]);
+
   return (
     <>
-      <button
+      {/* <button
         disabled={fileName === 'script.js'}
         onClick={() => setFileName('script.js')}
       >
@@ -24,13 +54,18 @@ const MonacoEditor = () => {
         onClick={() => setFileName('index.html')}
       >
         index.html
-      </button>
+      </button> */}
+      <div>{number}</div>
+      <button onClick={handleClick}>+</button>
       <Editor
         height="80vh"
         theme="vs-dark"
-        path={file.name}
-        defaultLanguage={file.language}
-        defaultValue={file.value}
+        // path="file:///E:/Learning/react-snowpack-typescript/src/project/file.html"
+        // path={res.url}
+        // defaultLanguage={file.language}
+        // defaultValue={file.value}
+        defaultLanguage="html"
+        defaultValue={fileName}
       />
     </>
   );
